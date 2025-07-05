0d08299750fcc03d721bc80fceb7fd15b9d02114
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-07-05 13:07:37 +0530

    updated

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 
 
-def rename_files_in_folder(folder_path, prefix="react-course-slide"):
+def rename_files_in_folder(folder_path, prefix=""):
     files = os.listdir(folder_path)
     files = [f for f in files if os.path.isfile(os.path.join(folder_path, f))]
 
     for index, filename in enumerate(files, start=1):
         name, ext = os.path.splitext(filename)
-        new_name = f"{prefix}-{index}{ext}"
+        new_name = f"{prefix}{index}{ext}"
         old_path = os.path.join(folder_path, filename)
         new_path = os.path.join(folder_path, new_name)
         os.rename(old_path, new_path)
@@ -15,7 +15,7 @@ def rename_files_in_folder(folder_path, prefix="react-course-slide"):
 
 
 # 📂 Replace this with your folder path
-folder_path = r"D:\course\Web Development\tut-web-react-js-from-maxschwarzmueller-udemy\Code\01 Intro of React"
+folder_path = r"D:\course\Web Development\tut-web-react-js-mu\Code\15(2) Quiz"
 rename_files_in_folder(folder_path)
 
 # files = [f for f in files if f.endswith(".jpg")]
